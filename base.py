ccc0574657a6be84c1c1c62ff2b97d028d9aff1c
porxy17 <ttli_16@windowslive.com>
2024-01-12 18:24:30 +0300

    Update base.py

--- base.py
+++ base.py
@@ -3,35 +3,35 @@ from deap import base, creator, tools, algorithms
 import random
 import numpy as np
 
-# Veri kümesini yükle
+
 excel_file_path = 'data.xlsx'
 dataset = pd.read_excel(excel_file_path)
 
-# Problemi tanımla
+
 creator.create('FitnessMin', base.Fitness, weights=(-1.0,))
 creator.create('Individual', list, fitness=creator.FitnessMin)
 
 toolbox = base.Toolbox()
 
-# Özellik oluşturucu: veri kümesine göre bireyler oluşturun
+
 def create_individual():
     individual_data = dataset.sample(n=1).iloc[0].tolist()
     return creator.Individual(individual_data)
 
-# Başlangıç popülasyonunu oluştur
+
 def init_population():
     population = [create_individual() for _ in range(20)]
     return population
 
 def eval_patient(individual):
     score = 0
-    # İlgili sütunların indeksini alın
+
     fractal_dimension_worst_idx = dataset.columns.get_loc('fractal_dimension_worst')
     symmetry_worst_idx = dataset.columns.get_loc('symmetry_worst')
     concave_points_worst_idx = dataset.columns.get_loc('concave_points_worst')
     concavity_worst_idx = dataset.columns.get_loc('concavity_worst')
     compactness_worst_idx = dataset.columns.get_loc('compactness_worst')
-    # Bireyin özelliklerine göre ceza veya ödül uygulayın
+  
     if float(individual[fractal_dimension_worst_idx]) > 0.1 :
         score += 40
     else:
@@ -55,7 +55,7 @@ def eval_patient(individual):
  
     return score,
 
-# Özel mutasyon işlevi
+
 def custom_mutate(individual, indpb):
     for i in range(len(individual)):
         if isinstance(individual[i], (float, np.float64)):
@@ -73,8 +73,7 @@ toolbox.register('select', tools.selTournament, tournsize=3)
 toolbox.register('individual', tools.initIterate, creator.Individual, create_individual)
 toolbox.register('population', init_population)
 
-# Elitizm ile Genetik Algoritma
-# Burada en iyi 20 birey hesaplamaya alınır ve bunların uygunluk değerleri hesaplanır ve oluşan yeni nesil ile eski nesil değişir
+
 def main():
     pop = toolbox.population()
     hof = tools.HallOfFame(20)
@@ -86,14 +85,14 @@ def main():
         pop = toolbox.select(offspring, len(pop))
         hof.update(pop)
 
-    # Taburcu uygunluğunun değerlendirilmesi
+    
     discharge_threshold = 0
     for i, patient in enumerate(hof):
         fitness = patient.fitness.values[0]
         if fitness <= discharge_threshold:
-            print(f"Patient {i+1} with fitness {fitness}: Bu hasta taburcu olabilir.")
+            print(f"Patient {i+1} with fitness {fitness}: this patient can be discharged")
         else:
-            print(f"Patient {i+1} with fitness {fitness}: Bu hastanın taburcu olma olasılığı düşük.")
+            print(f"Patient {i+1} with fitness {fitness}: this patient cannot be discharged")
 
     return pop, hof
 
